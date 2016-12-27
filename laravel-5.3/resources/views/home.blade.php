131f500f4b6e68ff51d02f3ab5402833f382ced0
truongbt <truongbt@atmarkcafe.org>
2016-12-27 08:58:53 +0700

    install passport

--- laravel-5.3/resources/views/home.blade.php
+++ laravel-5.3/resources/views/home.blade.php
@@ -8,6 +8,9 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
+                    <passport-clients></passport-clients>
+                    <passport-authorized-clients></passport-authorized-clients>
+                    <passport-personal-access-tokens></passport-personal-access-tokens>
                     You are logged in!
                 </div>
             </div>
